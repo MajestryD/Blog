27a5249b976e2a91e9997cbaa7d840e82a36a2e7
daniel pang <danielpang313@hotmail.com>
2022-06-16 15:40:44 -0400

    Finished Gatsby Tutorial Part 7

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-image","gatsby-plugin-sharp","gatsby-plugin-netlify-cms", "gatsby-plugin-sass", "gatsby-plugin-mdx", "gatsby-transformer-remark", {
+  plugins: ["gatsby-plugin-mdx","gatsby-transformer-sharp","gatsby-plugin-image","gatsby-plugin-sharp","gatsby-plugin-netlify-cms", "gatsby-plugin-sass", "gatsby-plugin-mdx", "gatsby-transformer-remark", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": `blog`,
---
+++ src/pages/blog/{mdx.slug}.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Layout from '../../components/layout';
+import {graphql} from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import {MDXRenderer} from 'gatsby-plugin-mdx';
+
+const BlogPost = ({ data }) => {
+
+  const image = getImage(data.mdx.frontmatter.hero_image)
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
+      <MDXRenderer>
+        {data.mdx.body}
+      </MDXRenderer>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($id: String) {
+    mdx(id: {eq: $id}) {
+      body
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`
+
+
+export default BlogPost;
